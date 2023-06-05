1d68fed720ddba899617133c0d2d2fcf1825f36d
Steve Wang <qian.wang.wg24@wharton.upenn.edu>
2023-06-05 15:49:03 +0800

    added sub step timers and outputs; separated timers by tests

--- zkevm-circuits/src/bytecode_circuit/dev.rs
+++ zkevm-circuits/src/bytecode_circuit/dev.rs
@@ -48,27 +48,37 @@ impl<F: Field> Circuit<F> for BytecodeCircuit<F> {
         use std::fs::OpenOptions;
         use std::io::prelude::*;
         use std::time::{Instant, Duration};
-        println!("Start timer");
-        let start = Instant::now();  // start timer
-
+        
+        println!("Start challenge timer");
+        let timer_challenge = Instant::now();  // start timer
         let challenges = challenges.values(&mut layouter);
+        let duration_challenge = timer_challenge.elapsed();  // end timer
 
+        println!("Start keccak timer");
+        let timer_keccak = Instant::now();  // start timer
         config.keccak_table.dev_load(
             &mut layouter,
             self.bytecodes.iter().map(|b| &b.bytes),
             &challenges,
         )?;
+        let duration_keccak = timer_keccak.elapsed();  // end timer
+
+        println!("Start synthesize sub timer");
+        let timer_synthesize_sub = Instant::now();  // start timer
         self.synthesize_sub(&config, &challenges, &mut layouter)?;
-
-        let duration = start.elapsed();  // end timer
-        println!("Time elapsed: {:?}", duration);
-
+        let duration_synthesize_sub = timer_synthesize_sub.elapsed();  // end timer
+        
+        let duration_total = duration_challenge + duration_keccak + duration_synthesize_sub;
+        println!("Total time elapsed: {:?}", duration_total);
         let mut file = OpenOptions::new()
         .write(true)
         .append(true)
         .create(true)
         .open("original_timer_result.txt")?;
-        writeln!(file, "Time elapsed for calling synthesize in the test is: {:?}", duration)?;
+        writeln!(file, "    Synthesize TOTAL {:?}", duration_total)?;
+        writeln!(file, "        challenge {:?}", duration_challenge)?;
+        writeln!(file, "        keccak table {:?}", duration_keccak)?;
+        writeln!(file, "        synthesize_sub {:?}", duration_synthesize_sub)?;
 
         Ok(())
     }
